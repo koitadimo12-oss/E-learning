befd7727083d81c0b9130045424319fb89b81c49
saliou-221-cloud <sg2297044@gmail.com>
2026-06-18 16:26:45 +0000

    chore: comment out missing AuthModule and UsersModule to allow compilation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+// import { AuthModule } from './auth/auth.module';
+// import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -21,9 +21,9 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
 
-    AuthModule,
+    // AuthModule,
 
-    UsersModule,
+    // UsersModule,
   ],
 })
 export class AppModule {}
