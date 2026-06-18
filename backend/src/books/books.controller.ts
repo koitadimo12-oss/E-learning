7f7ff9c17843a2a21fd8baf1d1ba134c18f846ee
Fatoumata <fatoumatadiarradabo387@gmail.com>
2026-06-18 19:49:54 +0000

    ajout du fichier books.controller.ts dans le dossier books

---
+++ backend/src/books/books.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { BooksService } from "./books.service";
+import { CreateBookDto } from "src/bookss/dto/create-book.dto";
+import { UpdateBookDto } from "src/bookss/dto/update-book.dto";
+
+@Controller('books')
+export class BooksController {
+
+ constructor(
+   private readonly booksService: BooksService
+ ) {}
+
+ @Post()
+ create(@Body() dto: CreateBookDto) {
+   return this.booksService.create(dto);
+ }
+
+ @Get()
+ findAll() {
+   return this.booksService.findAll();
+ }
+
+ @Get(':id')
+ findOne(@Param('id') id: string) {
+   return this.booksService.findOne(+id);
+ }
+
+ @Patch(':id')
+ update(
+   @Param('id') id: string,
+   @Body() dto: UpdateBookDto,
+ ) {
+   return this.booksService.update(+id, dto);
+ }
+
+ @Delete(':id')
+ remove(@Param('id') id: string) {
+   return this.booksService.remove(+id);
+ }
+}
