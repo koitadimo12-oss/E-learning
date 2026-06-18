ef6621a05128a0a896ff0d101478cb7c71b97a23
Fatoumata <fatoumatadiarradabo387@gmail.com>
2026-06-18 19:46:28 +0000

    ajout du fichier books.service.ts dans le dossier books

---
+++ backend/src/books/books.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Book } from "./entities/book.entity";
+import { Repository } from "typeorm";
+import { CreateBookDto } from "src/bookss/dto/create-book.dto";
+import { UpdateBookDto } from "src/bookss/dto/update-book.dto";
+
+@Injectable()
+export class BooksService {
+
+ constructor(
+   @InjectRepository(Book)
+   private bookRepository: Repository<Book>,
+ ) {}
+
+ create(dto: CreateBookDto) {
+   return this.bookRepository.save(dto);
+ }
+
+ findAll() {
+   return this.bookRepository.find();
+ }
+
+ findOne(id: number) {
+   return this.bookRepository.findOne({
+      where: { id }
+   });
+ }
+
+ update(id: number, dto: UpdateBookDto) {
+   return this.bookRepository.update(id, dto);
+ }
+
+ remove(id: number) {
+   return this.bookRepository.delete(id);
+ }
+}
