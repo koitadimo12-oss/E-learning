44e8d5f4fdb09c92c9c69f33dd3c52f5564d76e6
saliou-221-cloud <sg2297044@gmail.com>
2026-06-18 16:28:26 +0000

    feat: add CoursesService

---
+++ backend/src/courses/courses.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Course } from './entities/course.entity';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+
+@Injectable()
+export class CoursesService {
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {}
+
+  async create(createCourseDto: CreateCourseDto): Promise<Course> {
+    const course = this.courseRepository.create(createCourseDto);
+    return await this.courseRepository.save(course);
+  }
+
+  async findAll(): Promise<Course[]> {
+    return await this.courseRepository.find();
+  }
+
+  async findOne(id: number): Promise<Course> {
+    const course = await this.courseRepository.findOneBy({ id });
+    if (!course) {
+      throw new NotFoundException(`Le cours avec l'id ${id} n'existe pas.`);
+    }
+    return course;
+  }
+
+  async update(id: number, updateCourseDto: UpdateCourseDto): Promise<Course> {
+    const course = await this.findOne(id);
+    Object.assign(course, updateCourseDto);
+    return await this.courseRepository.save(course);
+  }
+
+  async remove(id: number): Promise<void> {
+    const course = await this.findOne(id);
+    await this.courseRepository.remove(course);
+  }
+}
