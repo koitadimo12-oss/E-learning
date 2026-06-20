6dd3118aa8e67def061d9ab23686b889895262bf
koitadimo12-oss <koitadimo12@gmail.com>
2026-06-20 16:22:40 +0300

    suppression de quelque fichier unitule

--- backend/src/enrollements/dto/update-enrollment.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEnrollmentDto } from './create-enrollment.dto';
-export class UpdateEnrollmentDto extends PartialType(
-  CreateEnrollmentDto,
-) {}
--- backend/src/enrollements/enrollments.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EnrollmentsController } from './enrollments.controller';
-import { EnrollmentsService } from './enrollments.service';
-
-@Module({
-  controllers: [EnrollmentsController],
-  providers: [EnrollmentsService],
-})
-export class EnrollmentsModule {}
